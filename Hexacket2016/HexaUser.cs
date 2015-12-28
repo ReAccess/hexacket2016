b0d150c7ad313a1b94ddd25095ec2c1e13c6121c
Michael SB <michaelsb1999@outlook.com>
2015-12-28 23:58:42 +0330

    Data

---
+++ Hexacket2016/HexaUser.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+
+namespace Hexacket2016
+{
+    class HexaUser
+    {
+        #region Data
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+
+        private int _authkey;
+        public int AuthKey
+        {
+            get { return _authkey; }
+            set { _authkey = value; }
+        }
+
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        private string _family;
+        public string Family
+        {
+            get { return _family; }
+            set { _family = value; }
+        }
+
+
+        private string _nickname;
+        public string Nickname
+        {
+            get { return _nickname; }
+            set { _nickname = value; }
+        }
+
+
+        private IPAddress _ip;
+        public IPAddress IP
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+        #endregion
+    }
+}
